b96437411b28ef46c10ea59f797d11cde1b64c80
ac1dst0rm <sven.gasser@digitalnatives.ch>
2019-02-17 16:34:17 +0100

    First commit
    basic webpage. no magic.

---
+++ webpage/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+<title>SeriveDesk Monitoring</title>
+<meta charset="UTF-8">
+<meta name="description" content="ServiceDesk Übersichtstafel / Monitoring Status Page">.
+<meta name="author" content="Sven Gasser">
+</header>
+
+<body>
+<div id="header">
+    <h1>Simple Site</h1>
+    <ul class="nav global">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Articles</a></li>
+        <li><a href="#">Portfolio</a></li>
+    </ul>
+ 
+</div>
---
+++ webpage/index.php
@@ -0,0 +1,17 @@
+<?php    
+    // Load config
+    require_once("/path/to/resources/config.php");
+     
+    require_once("/header.php");
+?>
+<div id="container">
+    <div id="content">
+       </div>
+    <?php
+        require_once("/content.php");
+    ?>
+</div>
+
+<?php
+    require_once("/footer.php");
+?>
